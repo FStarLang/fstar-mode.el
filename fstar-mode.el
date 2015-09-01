b456994f58304b62c64c0a2bdf181c6dda5efa08
Clément Pit--Claudel <clement.pitclaudel@live.com>
2015-09-01 00:33:42 -0700

    More syntax highlighting fixes

--- fstar-mode.el
+++ fstar-mode.el
@@ -69,6 +69,7 @@
                                 ("&&" . ?∧) ("||" . ?∨) ("<>" . ?≠)
                                 ("<==>" . ?⟺) ("==>" . ?⟹)
                                 ("=>" . ?⇒) ("->" . ?→)
+                                ;; ("(|" . 10629) ("|)" . 10630)
                                 ("'a" . ?α) ("'b" . ?β) ("'c" . ?γ)
                                 ("'d" . ?δ) ("'e" . ?η))
   "Fstar symbols.")
@@ -101,9 +102,9 @@
                 "effect" "new_effect" "sub_effect")
               'symbols))
 
-(defconst fstar-syntax-types
-  (regexp-opt '("Type")
-              'symbols))
+;; (defconst fstar-syntax-types
+;;   (regexp-opt '("Type")
+;;               'symbols))
 
 (defconst fstar-syntax-constants
   (regexp-opt '("False" "True")
@@ -116,18 +117,18 @@
   :group 'fstar)
 
 (defface fstar-subtype-face
-  '((t :inherit font-lock-type-face :italic t))
+  '((t :italic t))
   "Face used to highlight subtyping clauses."
   :group 'fstar)
 
 (defface fstar-attribute-face
   '((t :italic t))
-  "Face used to highlight subtyping clauses."
+  "Face used to highlight attributes."
   :group 'fstar)
 
 (defface fstar-decreases-face
   '((t :italic t))
-  "Face used to highlight subtyping clauses."
+  "Face used to highlight decreases clauses."
   :group 'fstar)
 
 (defvar-local should-cancel-match nil)
@@ -137,12 +138,12 @@
 
 Go back PREFIX-LEN chars, skip one sexp forward without moving
 point and report that position; point is placed at beginning of
-original match. If ALLOWED-NEWLINES is non-nil, then allow the
+original match.  If ALLOWED-NEWLINES is non-nil, then allow the
 sexp to span at most that many extra lines."
   (prog1
       (min (save-excursion
              (backward-char prefix-len)
-             (forward-sexp)
+             (ignore-errors (forward-sexp))
              (point))
            (save-excursion
              (forward-line (or allowed-newlines 0))
@@ -155,47 +156,60 @@ sexp to span at most that many extra lines."
 
 (defconst fstar-syntax-id (rx symbol-start
                               (? (or "#" "'"))
-                              letter (* (or wordchar (syntax symbol)))
+                              (any "a-z") (* (or wordchar (syntax symbol)))
                               (? "." (* (or wordchar (syntax symbol))))
                               symbol-end))
 
 (defconst fstar-syntax-cs (rx symbol-start
                               (any "A-Z") (* (or wordchar (syntax symbol)))
-                              (? ".v")
                               symbol-end))
 
 (defun fstar-find-id-with-type (bound)
-  (let ((start (point)) (found t) (rejected t))
+  (fstar-find-id-maybe-type bound t))
+
+(defun fstar-find-id-maybe-type (bound must-find-type)
+  (let ((found t) (rejected t))
     (while (and found rejected)
-      (setq found (re-search-forward (concat "\\(" fstar-syntax-id "\\) *:") bound t))
-      (setq rejected (and found (or (eq (char-after) ?:)
+      (setq found (re-search-forward (concat "\\(" fstar-syntax-id "\\) *\\(:\\)?") bound t))
+      (setq rejected (and found (or (eq (char-after) ?:) ; h :: t
+                                    (and must-find-type (not (match-beginning 2))) ; no type
                                     (save-excursion
                                       (goto-char (match-beginning 0))
                                       (skip-syntax-backward "-")
-                                      (or (eq (char-before) ?|)
+                                      (or (eq (char-before) ?|) ; | X: int
                                           (save-match-data
-                                            (looking-back "\\_<val" start))))))))
+                                            (looking-back "\\_<val" (point-at-bol))))))))) ; val x : Y:int
+    (when (and found (match-beginning 2))
+      (ignore-errors
+        (goto-char (match-end 2))
+        (forward-sexp)
+        (let ((md (match-data)))
+          (set-match-data `(,(car md) ,(point) ,@(cddr md))))))
     found))
 
-(defun fstar-id-with-type-pre-matcher ()
-  (or (save-excursion
-        (ignore-errors
-          (goto-char (match-beginning 0))
-          (skip-syntax-backward "-")
-          (when (eq (char-before) ?\()
-            (up-list)
-            (point))))
-      (save-excursion
-        (condition-case nil
-            (forward-sexp)
-          (error nil (forward-char)))
-        (point))))
+(defun fstar-find-id (bound)
+  (fstar-find-id-maybe-type bound nil))
+
+
+;; (defun fstar-id-with-type-pre-matcher ()
+;;   (or (save-excursion
+;;         (ignore-errors
+;;           (goto-char (match-beginning 0))
+;;           (skip-syntax-backward "-")
+;;           (when (eq (char-before) ?\()
+;;             (up-list)
+;;             (point))))
+;;       (save-excursion
+;;         (condition-case nil
+;;             (forward-sexp)
+;;           (error nil (forward-char)))
+;;         (point))))
 
 (defun fstar-find-fun-and-args (bound)
   (let ((found))
     (while (and (not found) (re-search-forward "\\_<fun\\_>" bound t))
       (-when-let* ((mdata (match-data))
-                   (fnd   (search-forward "->" bound t)))
+                   (fnd   (search-forward "->" bound t))) ;;FIXME
         (set-match-data `(,(car mdata) ,(match-end 0)
                           ,@mdata))
         (setq found t)))
@@ -214,11 +228,15 @@ sexp to span at most that many extra lines."
   (let ((id fstar-syntax-id))
     `(
       (fstar-find-subtype-annotation
-       ("\\({\\)\\(\\(?:.\\|\n\\)+\\)\\(}\\)"
-        (fstar-group-pre-matcher 2 2) nil
-        (1 'font-lock-type-face append)
-        (2 'fstar-subtype-face append)
-        (3 'font-lock-type-face append)))
+       ("{\\(\\(?:.\\|\n\\)+\\)}"
+        (fstar-group-pre-matcher 2 5) nil
+        (1 'fstar-subtype-face append))
+       ;; ("\\({\\)\\(\\(?:.\\|\n\\)+\\)\\(}\\)"
+       ;; (fstar-group-pre-matcher 2 5) nil
+       ;; (1 'font-lock-type-face append)
+       ;; (2 'fstar-subtype-face append)
+       ;; (3 'font-lock-type-face append))
+       )
       ("{:" (,(concat "\\({\\)\\(:" id "\\)\\(\\(?: +.+\\)?\\)\\(}\\)")
              (fstar-group-pre-matcher 2 2) nil
              (2 'font-lock-function-name-face append)
@@ -229,34 +247,37 @@ sexp to span at most that many extra lines."
       (,(concat "\\_<\\(let\\(?: +rec\\)?\\)\\(\\(?: +" id "\\)?\\) +[^=]+=")
        (1 'fstar-structure-face)
        (2 'font-lock-function-name-face)
-       (,id (fstar-subexpr-pre-matcher 2) nil
-            (0 'font-lock-variable-name-face)))
+       (fstar-find-id (fstar-subexpr-pre-matcher 2) nil
+                      (1 'font-lock-variable-name-face)))
       (fstar-find-id-with-type
        (1 'font-lock-variable-name-face)
-       (,id (fstar-id-with-type-pre-matcher) nil
-            (0 'font-lock-type-face)))
+       ;; (,id (fstar-id-with-type-pre-matcher) nil (0 'font-lock-type-face))
+       )
       (,(concat "\\_<\\(type\\|kind\\)\\( +" id "\\)\\s-*$")
        (1 'fstar-structure-face)
        (2 'font-lock-function-name-face))
       (,(concat "\\_<\\(type\\|kind\\)\\( +" id "\\)[^=]+=")
        (1 'fstar-structure-face)
        (2 'font-lock-function-name-face)
-       (,id (fstar-subexpr-pre-matcher 2) nil
-            (0 'font-lock-variable-name-face)))
-      ("\\(forall\\|exists\\) [^.]+\."
+       (fstar-find-id (fstar-subexpr-pre-matcher 2) nil
+                      (1 'font-lock-variable-name-face)))
+      ("\\_<\\(forall\\|exists\\) [^.]+\."
        (0 'font-lock-keyword-face)
-       (,id (fstar-subexpr-pre-matcher 1) nil
-            (0 'font-lock-variable-name-face)))
+       (fstar-find-id (fstar-subexpr-pre-matcher 1) nil
+                      (1 'font-lock-variable-name-face)))
       (fstar-find-fun-and-args
        (1 'font-lock-keyword-face)
-       (,id (fstar-subexpr-pre-matcher 1) nil
-            (0 'font-lock-variable-name-face)))
+       (fstar-find-id (fstar-subexpr-pre-matcher 1) nil
+                      (1 'font-lock-variable-name-face)))
       (,(concat "\\_<\\(val\\) +\\(" id "\\) *:")
        (1 'fstar-structure-face)
        (2 'font-lock-function-name-face)
-       (,id nil nil
-            (0 'font-lock-type-face)))
-      (,fstar-syntax-cs (0 'font-lock-constant-face)))))
+       ;; Type highlighting doesn't work too well
+       ;; (,id nil nil (0 'font-lock-type-face))
+       )
+      ;; Constructors and module calls use the same syntax
+      (,fstar-syntax-cs (0 'font-lock-constant-face))
+      )))
 
 ;;; Syntax table
 
@@ -313,7 +334,7 @@ sexp to span at most that many extra lines."
   "Setup font-lock for use with F*."
   (setq font-lock-defaults
         `(((,fstar-syntax-constants . 'font-lock-constant-face)
-           (,fstar-syntax-types     . 'font-lock-type-face)
+           ;; (,fstar-syntax-types     . 'font-lock-type-face)
            (,fstar-syntax-keywords  . 'font-lock-keyword-face)
            (,fstar-syntax-builtins  . 'font-lock-builtin-face)
            (,fstar-syntax-structure . 'fstar-structure-face)
@@ -327,13 +348,18 @@ sexp to span at most that many extra lines."
   (electric-indent-mode -1))
 
 ;;;###autoload
+
+;; (defun fstar-setup-comments ()
+  ;; (setq-local comment)
+
 (define-derived-mode fstar-mode prog-mode "F✪"
   :syntax-table fstar-syntax-table
   (fstar-setup-fontlock)
   (fstar-setup-prettify)
   (fstar-setup-indentation)
-  (flycheck-mode))
-
+  ;; (fstar-setup-comments)
+  ;; (flycheck-mode))
+  )
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.fs[ity]\\'" . fstar-mode))
 
