ec6938682fda9f020c791ea72e7325fef9c35601
Clément Pit--Claudel <clement.pitclaudel@live.com>
2015-09-08 20:18:59 -0400

    Minor fixes after merging #5

--- fstar-mode.el
+++ fstar-mode.el
@@ -48,7 +48,7 @@
 (defmacro fstar-assert (&rest args)
   "Call `cl-assert' on ARGS, if available."
   (declare (debug t))
-  `(when (fboundp 'assert)
+  `(when (fboundp 'cl-assert)
      (cl-assert ,@args)))
 
 ;;; Group
@@ -475,7 +475,7 @@ If PROC is nil, use the current buffer's `fstar-subp--process'."
   "Same as `fstar-subp-kill', but for PROC instead of `fstar-subp--process'."
   (fstar-subp-with-source-buffer proc
     (when fstar-subp--process
-      (cl-assert (eq proc fstar-subp--process))
+      (fstar-assert (eq proc fstar-subp--process))
       (fstar-subp-kill))))
 
 (defun fstar-subp-sentinel (proc signal)
