68ab3d93b24371e4052190296ef46ea5731d4216
Syohei YOSHIDA <syohex@gmail.com>
2016-02-12 11:30:27 +0900

    Clean up dependencies
    cl-lib was bundled since Emacs 24.3.

--- fstar-mode.el
+++ fstar-mode.el
@@ -6,7 +6,7 @@
 
 ;; Created: 27 Aug 2015
 ;; Version: 0.2.2
-;; Package-Requires: ((emacs "24.3") (cl-lib "0.3") (dash "2.11"))
+;; Package-Requires: ((emacs "24.3") (dash "2.11"))
 ;; Keywords: convenience, languages
 
 ;; This file is not part of GNU Emacs.
