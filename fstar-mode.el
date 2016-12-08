FStarLang/fstar-mode.el

commit 2fc3665fe16399674e476b2284f968a18b628687

Remove an extraneous blank line and count lines from 0

fstar-mode.el
@@ -874,8 +874,8 @@ multiple arguments as one string will not work: you should use
 (defun fstar-subp--header (pos lax)
   "Prepare a header for a region starting at POS.
 With non-nil LAX, the region is to be processed in lax mode."
-  (format "#push %d %d%s\n"
-          (line-number-at-pos pos)
+  (format "#push %d %d%s"
+          (1- (line-number-at-pos pos))
           (fstar-subp--column-number-at-pos pos)
           (if lax " #lax" "")))
 

commit f72d3e15d0776c9367c17fa7436e916087447d7c

Fix incorrect line offset introduced in 2fc3665

fstar-mode.el
@@ -875,7 +875,7 @@ multiple arguments as one string will not work: you should use
   "Prepare a header for a region starting at POS.
 With non-nil LAX, the region is to be processed in lax mode."
   (format "#push %d %d%s"
-          (1- (line-number-at-pos pos))
+          (line-number-at-pos pos)
           (fstar-subp--column-number-at-pos pos)
           (if lax " #lax" "")))
 