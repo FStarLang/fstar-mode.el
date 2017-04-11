92f3a17747914169fc246fe6932e620b982a7a56
Clément Pit--Claudel <clement.pitclaudel@live.com>
2017-04-11 19:40:37 -0400

    Propagate detected version number to buffer of F* subprocess

--- fstar-mode.el
+++ fstar-mode.el
@@ -2353,11 +2353,13 @@ error referring to PROG as PROG-NAME and VAR-NAME."
 
 (defun fstar-subp-make-buffer ()
   "Create a buffer for the F* subprocess."
-  (with-current-buffer (generate-new-buffer
-                        (format " *F* interactive for %s*" (buffer-name)))
-    (add-hook 'kill-buffer-hook #'fstar-subp-buffer-killed t t)
-    (buffer-disable-undo)
-    (current-buffer)))
+  (let ((vernum fstar--vernum))
+    (with-current-buffer (generate-new-buffer
+                          (format " *F* interactive for %s*" (buffer-name)))
+      (add-hook 'kill-buffer-hook #'fstar-subp-buffer-killed t t)
+      (setq fstar--vernum vernum)
+      (buffer-disable-undo)
+      (current-buffer))))
 
 (defcustom fstar-subp-prover-args nil
   "Used for computing arguments to pass to F* in interactive mode.
