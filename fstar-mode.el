FStarLang/fstar-mode.el

commit eab962961175ad74d3c06881b4949c81bc37a4f4

tramp: Drop uses of tramp-make-tramp-file-name
The signature of that method changed recently, and it's apparently intended to
be internal.

Take that opportunity to refactor fstar-find-executable.

fstar-mode.el
@@ -225,11 +225,6 @@ after."
       (string-trim-right (car (process-lines "cygpath" "-w" path)))
     path))
 
-(defun fstar--make-tramp-file-name (fname)
-  "Convert FNAME to a name on remote host."
-  (with-parsed-tramp-file-name buffer-file-name nil
-    (tramp-make-tramp-file-name method user host fname)))
-
 (defun fstar--tramp-find-executable (prog)
   "Check if PROG is in the remote path."
   (with-parsed-tramp-file-name buffer-file-name vec
@@ -4292,7 +4287,8 @@ PROG-NAME and VAR-NAME are used in error messages."
                 (if var-name (format "; please adjust `%s'" var-name) "")))
   (unless (file-executable-p path)
     (user-error "%s (“%s”) not executable%s" prog-name path
-                (if var-name (format "; please check `%s'" var-name) ""))))
+                (if var-name (format "; please check `%s'" var-name) "")))
+  path)
 
 (defun fstar-find-executable (prog prog-name &optional var-name local-only)
   "Compute the absolute path to PROG.
@@ -4300,16 +4296,16 @@ Check that the binary exists and is executable; if not, raise an
 error referring to PROG as PROG-NAME and VAR-NAME.  This function
 finds a remote binary when the current buffer is a Tramp file,
 unless LOCAL-ONLY is set."
-  (let* ((local (or local-only (not (fstar--remote-p))))
-         (abs (if local (executable-find prog) prog)))
+  (let* ((local (or local-only (not (fstar--remote-p)))))
     (if local
-        (fstar--check-executable (or abs prog) prog-name var-name)
-      (with-parsed-tramp-file-name buffer-file-name nil
-        (or (tramp-find-executable v abs nil)
+        (fstar--check-executable (executable-find prog) prog-name var-name)
+      (let ((tramp-vect (tramp-dissect-file-name buffer-file-name))
+            (prog-name (concat "Remote " prog-name)))
+        (if (file-name-absolute-p prog)
             (fstar--check-executable
-             (tramp-make-tramp-file-name method user host abs)
-             (concat "Remote " prog-name) var-name))))
-    abs))
+             (concat (file-remote-p buffer-file-name) prog)
+             prog-name var-name)
+          (tramp-find-executable tramp-vect prog nil))))))
 
 (defun fstar-subp-buffer-killed ()
   "Kill F* process associated to current buffer."

commit b03a5c9f69260c7620db9732f8477aa19e06c9bc

Fix Tramp breakage introduced by eab9629
Reported-by: @kyodralliam

fstar-mode.el
@@ -4584,16 +4584,18 @@ Check that the binary exists and is executable; if not, raise an
 error referring to PROG as PROG-NAME and VAR-NAME.  This function
 finds a remote binary when the current buffer is a Tramp file,
 unless LOCAL-ONLY is set."
-  (let* ((local (or local-only (not (fstar--remote-p)))))
-    (if local
-        (fstar--check-executable (or (executable-find prog) prog) prog-name var-name)
-      (let ((tramp-vect (tramp-dissect-file-name buffer-file-name))
-            (prog-name (concat "Remote " prog-name)))
-        (if (file-name-absolute-p prog)
-            (fstar--check-executable
-             (concat (file-remote-p buffer-file-name) prog)
-             prog-name var-name)
-          (tramp-find-executable tramp-vect prog nil))))))
+  (-if-let* ((local (or local-only (not (fstar--remote-p)))))
+      (fstar--check-executable (or (executable-find prog) prog) prog-name var-name)
+    (let ((tramp-vect (tramp-dissect-file-name buffer-file-name))
+          (prog-name (concat "Remote " prog-name)))
+      (if (file-name-absolute-p prog)
+          (fstar--check-executable
+           (concat (file-remote-p buffer-file-name) prog)
+           prog-name var-name)
+        (when (tramp-find-executable tramp-vect prog nil)
+          ;; We don't return the output of `tramp-find-executable' because it is
+          ;; "\\PROG" when PROG is in $PATH, which confuses `process-file'.
+          prog)))))
 
 (defun fstar-subp-buffer-killed ()
   "Kill F* process associated to current buffer."