a0b8d235cc9113d291db0555a05e4b8ff98ffb6c
Clément Pit--Claudel <clement.pitclaudel@live.com>
2018-03-09 01:56:50 -0500

    Refactor -write-transcript

--- fstar-mode.el
+++ fstar-mode.el
@@ -622,26 +622,31 @@ FORMAT and ARGS are as in `message'."
       (insert line "\n"))
     (write-region (point-min) (point-max) fname)))
 
+(defun fstar-write-transcript-1 (prefix)
+  "Write latest transcript in current buffer to PREFIX.IN and PREFIX.OUT."
+  (interactive "FSave transcript as: ")
+  (goto-char (point-max))
+  (unless (re-search-backward "^;;; \n" nil t)
+    (user-error "Could not find a complete transcript to save"))
+  (let ((log (buffer-substring-no-properties (match-end 0) (point-max)))
+        (exclude-re (concat "^" (regexp-opt '(">>> " "!!! " ";;; "))))
+        (in nil) (out nil))
+    (dolist (line (delete "" (split-string log "\n")))
+      (cond
+       ((string-match "^>>> " line)
+        (push (substring line (match-end 0)) in))
+       ((not (string-match-p exclude-re line))
+        (unless (equal line "") (push line out)))))
+    (fstar--write-transcript-1 (concat prefix ".in") in)
+    (fstar--write-transcript-1 (concat prefix ".out") out)))
+
 (defun fstar-write-transcript (prefix)
-  "Write latest transcript to a PREFIX.IN and PREFIX.OUT."
+  "Write latest transcript to PREFIX.IN and PREFIX.OUT."
   (interactive "FSave transcript as: ")
   (unless fstar-debug
     (user-error "Use `fstar-toggle-debug' to collect traces"))
   (with-current-buffer (fstar--log-buffer)
-    (goto-char (point-max))
-    (unless (re-search-backward "^;;; \n" nil t)
-      (user-error "Could not find a complete transcript to save"))
-    (let ((log (buffer-substring-no-properties (match-end 0) (point-max)))
-          (exclude-re (concat "^" (regexp-opt '(">>> " "!!! " ";;; "))))
-          (in nil) (out nil))
-      (dolist (line (delete "" (split-string log "\n")))
-        (cond
-         ((string-match "^>>> " line)
-          (push (substring line (match-end 0)) in))
-         ((not (string-match-p exclude-re line))
-          (unless (equal line "") (push line out)))))
-      (fstar--write-transcript-1 (concat prefix ".in") in)
-      (fstar--write-transcript-1 (concat prefix ".out") out))))
+    (fstar-write-transcript-1 prefix)))
 
 ;;; Compatibility across F* versions
 
